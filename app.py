a8af94ba8b8d796737ea461d20e4bbef2d2f5f68
ashengtx <linliansheng007@163.com>
2016-09-19 15:27:37 +0800

    加入生词的反向确认

--- app.py
+++ app.py
@@ -9,15 +9,12 @@ from backup import backup_vocab
 
 
 LETTER = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ERROR_INSTRUCTION = '请输入正确的指令！'
 INPUT_TEXT = ''
 
 def get_vocab(filename):
     with open(filename, 'rb') as fpk:
         return pickle.load(fpk)
-
-def fuck(filename):
-    print('fuck ', filename)
-    return
 
 def get_words_from_txt(filename):
     with open(filename, 'r', encoding='utf8') as fl:
@@ -35,58 +32,127 @@ def get_words_from_txt(filename):
             ret.append(word)
     return sorted(set(ret))
 
-def get_new_words(words, mastered):
-    return [word for word in words if word not in mastered]
-
 def verification(words):
     mastered = []
+    unmastered = []
+    input_mastered = []
+    input_unmastered = []
     new_words = words
-    op = ''
-    # 输入0000表示确认完毕
-    while op != '0000':
-        print('new_words: ')
-        print(new_words)
-        print('mastered: ')
-        print(mastered)
-        print('是否所有的new_words都是生词？(y/n)')
+
+    print('new_words: ')
+    print(new_words)
+
+    message = '''
+---------------------------------
+|    若已掌握的词比较少，输入1  |
+|    若生词比较少，输入2        |
+---------------------------------
+    '''
+    while True:
+        print(message)
         op = input("word_app/recognize/veirfy>")
-        # 如果输入y和n以外的命令，则循环
-        while (op != 'y' and op !='n'):
-            print('是否所有的new_words都是生词？(请输入y或者n)(y/n)')
-            op = input("word_app/recognize/veirfy>")
-        if op == 'n':
-            input_str = input("请输入已掌握的单词，用空格隔开：")
-            input_words = input_str.split()
-            # 如果输入的词不在new_words里面，则再次输入
-            flag = False
-            wrong_word = []
-            for w in input_words:
-                if w not in new_words:
-                    wrong_word.append(w)
-                    flag = True
-            if flag:
-                print('你输入了未出现的单词: ')
-                print(wrong_word)
-            else:
-                for w in input_words:
-                    new_words.remove(w)
-                    mastered.append(w)
-        if op == 'y':
+        if op == '1':
+            input_mastered = verify_mastered(new_words)
+            for w in input_mastered:
+                mastered.append(w)
+            unmastered = new_words
             break
-    return (new_words, mastered)
+        elif op == '2':
+            input_unmastered = verify_unmastered(new_words)
+            for w in input_unmastered:
+                unmastered.append(w)
+            mastered = new_words
+            break
+        else:
+            print(ERROR_INSTRUCTION)
+    return (mastered, unmastered)
+
+def verify_mastered(words):
+    mastered = []
+    new_words = words
+    while True:
+        print('是否所有的new_words都是生词？(y/n)')
+        op = input("word_app/recognize/verify>")
+        if op == 'n':
+            # 输入已掌握的词，或者生词
+            while True:
+                input_str = input("请输入已掌握的单词，用空格隔开：")
+                input_words = input_str.split()
+                # 如果输入的词不在new_words里面，则再次输入
+                flag = False
+                wrong_word = []
+                for w in input_words:
+                    if w not in new_words:
+                        wrong_word.append(w)
+                        flag = True
+                if flag:
+                    print('你输入了未出现的单词: ')
+                    print(wrong_word)
+                else:
+                    for w in input_words:
+                        mastered.append(w)
+                        new_words.remove(w)   
+            print('new_words: ')
+            print(new_words)
+            print('mastered: ')
+            print(mastered)      
+        elif op == 'y':
+            return mastered
+        else:
+            print(ERROR_INSTRUCTION)
+
+def verify_unmastered(words):
+    unmastered = []
+    new_words = words
+    while True:
+        print('是否所有的new_words都已掌握？(y/n)')
+        op = input("word_app/recognize/verify>")
+        if op == 'n':
+            # 输入生词
+            while True:
+                input_str = input("请输入生词，用空格隔开：")
+                input_words = input_str.split()
+                # 如果输入的词不在new_words里面，则再次输入
+                flag = False
+                wrong_word = []
+                for w in input_words:
+                    if w not in new_words:
+                        wrong_word.append(w)
+                        flag = True
+                if flag:
+                    print('你输入了未出现的单词: ')
+                    print(wrong_word)
+                else:
+                    for w in input_words:
+                        unmastered.append(w)
+                        new_words.remove(w)
+                    break
+            print('new_words: ')
+            print(new_words)
+            print('unmastered: ')
+            print(unmastered)       
+        elif op == 'y':
+            return unmastered
+        else:
+            print(ERROR_INSTRUCTION)
+
 
 def welcome():
     '''
     menu
     '''
     welcome_message = '''
-    欢迎使用生词识别app！！！
+--------------------------------------
+|       欢迎使用生词识别app!!!       |     
+--------------------------------------
     '''
     menu = '''
-    输入以下命令执行相应功能：
-    v/vocab         显示已掌握及待掌握的单词
-    r/recognize     开始生词识别
-    q/quit          退出
+--------------------------------------
+|    输入以下命令执行相应功能：      |
+|    v/vocab         显示词汇表      |
+|    r/recognize     开始生词识别    |
+|    q/quit          退出            |
+--------------------------------------
     '''
     print(welcome_message)
     while True:
@@ -108,7 +174,15 @@ def recognize():
     生词识别
     '''
     # 获得文章里的单词
-    print('请输入article目录下的英文文章名字，m或menu返回主菜单，q或quit退出')
+    message = '''
+------------------------------------------
+|    请输入article目录下的英文文章名字   |
+|                                        |
+|    m或menu返回主菜单                   |
+|    q或quit退出                         |
+------------------------------------------
+    '''
+    print(message)
     filename = input('word_app/recognize>')
     if filename in ['q', 'quit']:
         quit()
@@ -127,19 +201,19 @@ def recognize():
     mastered = [w for w in vocab if vocab[w] >= 10]
 
     # 获得未掌握的单词，即value<10，
-    new_words = get_new_words(words, mastered)
+    new_words = [word for word in words if word not in mastered]
 
     # 让用户确认这些单词是否都是未掌握的
-    new_words, mastered = verification(new_words)
+    mastered, unmastered = verification(new_words)
 
     # 更新vocab
-    for w in new_words:
-        vocab[w] += 1
+    for w in unmastered:
+        vocab[w] = 1
     for w in mastered:
         vocab[w] = 10
 
-    print('在这篇文章里你一共遇到了{}个生词:'.format(len(new_words)))
-    print(new_words)
+    print('在这篇文章里你一共遇到了{}个生词:'.format(len(unmastered)))
+    print(unmastered)
     print('你的词汇表又增加了{}个已掌握的单词:'.format(len(mastered)))
     print(mastered)
     
@@ -172,9 +246,11 @@ def vocab():
 
     print('你的词汇表当前一共有{0}个单词，其中已掌握{1}个，还有{2}个待掌握。'.format(len(vocab), len(mastered), len(unmastered)))
     message = '''
-    s/show          显示词汇表
-    m/menu          返回主菜单
-    r/recognize     开始生词识别
+--------------------------------------
+|    s/show          显示词汇表      |
+|    m/menu          返回主菜单      |
+|    r/recognize     开始生词识别    |
+--------------------------------------    
     '''
     print(message)
     op = input('word_app/vocab>')
